0c23e3d6d46b91adce842769ffd244caa49784a7
Antoine Chagnon Michaud <achagnonmichaud@cvm.qc.ca>
2023-02-21 20:16:14 -0500

    Working version of FAN.ino>

--- Fan/Fan.ino
+++ Fan/Fan.ino
@@ -2,10 +2,8 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
-#include <FanController.h>
 
-#define FAN_O_PIN 5
-#define FAN_I_PIN 2
+#define FAN_PIN 5
 #define DOWNSTAIR_DHT_PIN 7
 #define UPSTAIR_DHT_PIN 8
 #define DHTTYPE DHT11
@@ -13,13 +11,13 @@
 #define BUZZER_PIN 9
 uint32_t MAIN_LOOP_DELAY = 1000;
 const float DESIRED_TEMPERATURE = 20.00;
-bool fanIsOn = false;
+const uint8_t MAX_FAN_SPEED = 125;
+const uint8_t MIN_FAN_SPEED = 40;
 
 // Wiring: SDA pin is connected to A4 and SCL pin to A5.
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2); // Change to (0x27,20,4) for 20x4 LCD.
 DHT dhtDownstair = DHT(DOWNSTAIR_DHT_PIN, DHTTYPE);
 DHT dhtUpstair = DHT(UPSTAIR_DHT_PIN, DHTTYPE);
-FanController fan(FAN_I_PIN, 1000, FAN_O_PIN);
 
 void setup() {
   Serial.begin(9600);
@@ -27,13 +25,13 @@ void setup() {
   initDhtSensor(dhtUpstair);
   initLCD();
   //init fan
-  fan.begin();
+  pinMode(FAN_PIN, OUTPUT);
 }
 
 void initLCD() {
   Serial.println(F("INIT LCD"));
   lcd.init();
-  lcd.noBacklight();
+  lcd.backlight();
 }
 
 void initDhtSensor(DHT &dht)
@@ -42,51 +40,24 @@ void initDhtSensor(DHT &dht)
   dht.begin();
   getTemperature(dht);
 }
-
 void loop() {
-  unsigned int rpms = fan.getSpeed(); // Send the command to get RPM
-  Serial.print(rpms);
-  Serial.println("RPM");
-  analogWrite(FAN_O_PIN, 2.25*25);
-  Serial.println("speed is at 25");
-  delay(20000);
-  analogWrite(FAN_O_PIN, 2.25*100);
-  Serial.println("speed is at 100");
-  delay(20000);
-  analogWrite(FAN_O_PIN, 2.25*50);
-  Serial.println("speed is at 50");
-  delay(20000);
-  analogWrite(FAN_O_PIN, 2.25*0);
-  Serial.println("speed is at 0");
-  delay(20000);
+  float temperatureUpstair = getTemperature(dhtUpstair);
+  float temperatureDownstair = getTemperature(dhtDownstair);
+
+  uint16_t speed = 0;
+  if (temperatureDownstair > temperatureUpstair && temperatureUpstair < DESIRED_TEMPERATURE)
+  {
+    speed = (int)(temperatureDownstair-temperatureUpstair)*40;
+    speed = min(MAX_FAN_SPEED, max(MIN_FAN_SPEED, speed));
+  }
+  
+  analogWrite(FAN_PIN, speed);
+
+  printLCD("UPS " + String(temperatureUpstair, 2) + " | " + String(speed), 0);
+  printLCD("DOWNSTAIR " + String(temperatureDownstair, 2), 1);
+
+  delay(MAIN_LOOP_DELAY);
 }
-
-// void loop() {
-//   float temperatureUpstair = getTemperature(dhtUpstair);
-//   float temperatureDownstair = getTemperature(dhtDownstair);
-//   printLCD("UPSTAIR " + String(temperatureUpstair, 2) + " C", 0);
-//   printLCD("DOWNSTAIR " + String(temperatureDownstair, 2) + " C", 1);
-
-//   if (fanIsOn)
-//   {
-//     bool downstairIsTooCold = temperatureDownstair <= DESIRED_TEMPERATURE - 0.25;
-//     bool temperatureUpstairIsConfortable = temperatureUpstair >= DESIRED_TEMPERATURE + 0.25;
-//     if (temperatureUpstairIsConfortable && downstairIsTooCold)
-//     {
-//       turnOffFan();
-//     }
-//   }
-//   else
-//   {
-//     bool downstairIsHotEnough = temperatureDownstair > DESIRED_TEMPERATURE;
-//     bool upstairIsCold = temperatureUpstair < DESIRED_TEMPERATURE;
-//     if (downstairIsHotEnough && upstairIsCold)
-//     {
-//       turnOnFan(1);
-//     }
-//   }
-//   delay(MAIN_LOOP_DELAY);
-// }
 
 float getTemperature(DHT &dht)
 {
